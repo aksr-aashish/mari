e8e520271f7718828f6bb4709a6d59d78f4fd540
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-22 23:11:27 +0530

    Update userinfo.py

--- AsunaRobot/modules/userinfo.py
+++ AsunaRobot/modules/userinfo.py
@@ -461,7 +461,7 @@ def set_about_me(update: Update, context: CallbackContext):
 def stats(update: Update, context: CallbackContext):
     stats = "<b>『✨komi analytics』:</b>\n" + "\n".join([mod.__stats__() for mod in STATS])
     result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-    update.effective_message.reply_text(result, parse_mode=ParseMode.HTML)
+    update.effective_message.reply_text(result, parse_mode=ParseMode.HTML,disable_web_page_preview=True)
 
 
 
