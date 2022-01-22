2c2a2988648e61ab7e578a0c649a8947121502bb
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-22 20:21:12 +0530

    Add files via upload

---
+++ AsunaRobot/arqclient.py
@@ -0,0 +1,9 @@
+from aiohttp import ClientSession
+from Python_ARQ import ARQ
+from AsunaRobot import ARQ_API_KEY
+
+ARQ_API_URL = "https://grambuilders.tech"
+
+aiohttpsession = ClientSession()
+
+arq = ARQ(ARQ_API_URL, ARQ_API_KEY, aiohttpsession)
