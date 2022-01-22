32f7bfe4ce16ef629e766762b8b3b6700d570287
Aarukami <91074890+Aarukami@users.noreply.github.com>
2022-01-23 02:47:57 +0530

    Update misc.py

--- AsunaRobot/modules/helper_funcs/misc.py
+++ AsunaRobot/modules/helper_funcs/misc.py
@@ -1,3 +1,5 @@
+[In reply to Sɪɴxɪᴏᴜs]
+from math import ceil
 from typing import Dict, List
 
 from AsunaRobot import NO_LOAD
@@ -6,13 +8,13 @@ from telegram.error import TelegramError
 
 
 class EqInlineKeyboardButton(InlineKeyboardButton):
-    def __eq__(self, other):
+    def eq(self, other):
         return self.text == other.text
 
-    def __lt__(self, other):
+    def lt(self, other):
         return self.text < other.text
 
-    def __gt__(self, other):
+    def gt(self, other):
         return self.text > other.text
 
 
@@ -39,40 +41,38 @@ def split_message(msg: str) -> List[str]:
 def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__mod_name__,
-                    callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.lower()
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
+            [EqInlineKeyboardButton(x.mod_name,
+                                    callback_data="{}_module({})".format(prefix, x.mod_name.lower())) for x
+             in module_dict.values()])
     else:
         modules = sorted(
-            [
-                EqInlineKeyboardButton(
-                    x.__mod_name__,
-                    callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.lower()
-                    ),
-                )
-                for x in module_dict.values()
-            ]
-        )
+            [EqInlineKeyboardButton(x.mod_name,
+                                    callback_data="{}_module({},{})".format(prefix, chat, x.mod_name.lower())) for x
+             in module_dict.values()])
 
-    pairs = [modules[i * 4 : (i + 1) * 4] for i in range((len(modules) + 4 - 1) // 4)]
+    pairs = [
+    modules[i * 3:(i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)
+    ]
 
-    round_num = len(modules) / 4
+    round_num = len(modules) / 3
     calc = len(modules) - round(round_num)
-    if calc in [1, 2]:
-        pairs.append((modules[-1],))
+    if calc == 1:
+        pairs.append((modules[-1], ))
     elif calc == 2:
-        pairs.append((modules[-1],))
+        pairs.append((modules[-1], ))
+
+    max_num_pages = ceil(len(pairs) / 10)
+    modulo_page = page_n % max_num_pages
+
+    # can only have a certain amount of buttons side by side
+    if len(pairs) > 8:
+        pairs = pairs[modulo_page * 8:8 * (modulo_page + 1)] + [
+            (EqInlineKeyboardButton("⬅️", callback_data="{}_prev({})".format(prefix, modulo_page)),
+                EqInlineKeyboardButton("Back", callback_data="layla_back"),
+             EqInlineKeyboardButton("➡️", callback_data="{}_next({})".format(prefix, modulo_page)))]
 
     else:
-        pairs += [[EqInlineKeyboardButton("「 GO BACK TO MAIN MENU 」", callback_data="kazuko_back")]]
+        pairs += [[EqInlineKeyboardButton("Back", callback_data="layla_back")]]
 
     return pairs
 
